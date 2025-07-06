6db712614540263da9dcd3853f85f14c43db9ade
notbenwang <112118953+notbenwang@users.noreply.github.com>
2025-07-06 00:07:02 -0400

    more changes

--- src/app/components/Crew.js
+++ src/app/components/Crew.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import { useState } from 'react';
 
-export default function Crew({gothicFont}) {
+export default function Crew({font}) {
     const [hoveredIndex, setHoveredIndex] = useState(null);
     
     const images = [
@@ -13,43 +13,43 @@ export default function Crew({gothicFont}) {
     ];
 
     const members = [
-        "MEMBER 01",
-        "MEMBER 02",
-        "MEMBER 03",
-        "MEMBER 04",
+        "MEMBER 01 •",
+        "MEMBER 02 •",
+        "MEMBER 03 •",
+        "MEMBER 04 •",
     ]
 
     return (
         
-    <div className="grid grid-cols-2 px-4 justify-items-center outline outline-gray-300 w-screen max-w-4xl">
-        <div className="relative w-full max-w-xl aspect-[1/1] mt-5 h-screen">
+    <div className="grid grid-cols-2 justify-items-center outline outline-gray-300 w-[60vw] mr-auto max-w-7xl shadow-xl p-5">
+
+        <div className={`${font.className} text-lg text-right`}>
+            <h1 className="text-black font-bold text-[clamp(1.25rem,4vw,10rem)]">THE CREW</h1>
+            {members.map((member, index) => (
+                    <div>
+                    <h4 
+                        key={member} 
+                        className={`inline-block text-gray-800 hover:text-black hover:scale-115 text-[clamp(0.25rem,2.5vw,1.5rem)] transition }`}
+                        onMouseEnter={() => setHoveredIndex(3-index)}
+                        onMouseLeave={() => setHoveredIndex(null)}
+                    >
+                        {member}
+                    </h4>
+                    </div>
+                ))
+            }
+        
+            <h4 className="text-gray-300 inline-block text-right text-[clamp(0.25rem,2.5vw,1.5rem)]">MEMBER 05 •</h4><br/>
+            <h4 className="text-gray-300 inline-block text-right text-[clamp(0.25rem,2.5vw,1.5rem)]">MEMBER 06 •</h4><br/>
+            <h4 className="text-gray-300 inline-block text-right text-[clamp(0.25rem,2.5vw,1.5rem)]">MEMBER 07 •</h4><br/>
+        </div>
+            <div className="relative w-full max-w-lg aspect-[3/4] bg-linear-to-b from-slate-100 via-slate-400 to-slate-900 shadow-2xl rounded-xl">
             {images.map((image, index) => (
-                <img key={image} src={image} className={`absolute transition ${hoveredIndex === index ? 'scale-105' : 'scale-100 brightness-75'} z-${4-index}`}/>
+                <img key={image} src={image} className={`absolute transition ${hoveredIndex === index ? 'scale-110' : 'scale-105 brightness-85'} z-${4-index}`}/>
             
             ))}
         </div>
-        <div className={`${gothicFont.className} text-lg text-right mr-5`}>
-        <br/>
-        <h1 className="text-black font-bold text-7xl">THE CREW</h1>
-        {members.map((member, index) => (
-                <div>
-                <h4 
-                    key={3-index} 
-                    className={`inline-block text-black hover:scale-125 transition }`}
-                    onMouseEnter={() => setHoveredIndex(3-index)}
-                    onMouseLeave={() => setHoveredIndex(null)}
-                >
-                    {member}
-                </h4>
-                <br/>
-                </div>
-            ))
-        }
         
-        <h4 className="text-gray-300 inline-block text-right">MEMBER 05</h4><br/>
-        <h4 className="text-gray-300 inline-block text-right">MEMBER 06</h4><br/>
-        <h4 className="text-gray-300 inline-block text-right">MEMBER 07</h4><br/>
-        </div>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -17,15 +17,25 @@ export default function Home() {
       <img src="erebo.svg" className="absolute inset-0 w-full h-full object-cover"alt="Background Image" />
       <div className="absolute top-0 translate-y-1/8 flex flex-col items-center justify-start gap-8 ">
         <img src="logo.png" className="object-cover p-5 z-15" alt="Logo" />
-    
-        <div className="flex flex-col items-center justify-center w-screen  mt-[10vh] bg-white ">
-
-          <div className="m-5 grid grid-cols-3 gap-4 px-8 justify-items-center max-w-8xl"> 
-            <img src="01.png" className="rounded-xl z-10 scale-95 hover:scale-100 transition w-full max-w-lg duration-200"/>
-            <img src="02.png" className="rounded-xl z-10 scale-95 hover:scale-100 transition w-full max-w-lg duration-200"/>
-            <img src="03.png" className="rounded-xl z-10 scale-95 hover:scale-100 transition w-full max-w-lg duration-200"/>
+        <div className="flex flex-col items-left justify-center w-screen mt-[10vh] bg-white ">
+          <p className={`font-mono  text-black m-5 px-8 w-3/5 max-w-3xl outline outline-gray-300 pb-8 shadow-lg text-[clamp(0.75rem,2vw,1.25rem)]`}>
+            <br/>
+            <a className={`${gothicFont.className} font-bold text-3xl`}>Introduction</a> 
+            <br/>
+            The Floaters are a group of explorers journeying to be the first in millenia to breach <a className="italic">Erebo</a>, the storm of darkness that surrounds their alien world. 
+            <br/><br/>
+            <a className="text-black/50 hover:text-black transition duration-200" href="">
+              Read More →
+            </a>
+            </p>
+          <div className="mx-5 grid grid-cols-3 gap-4 px-8 justify-items-center max-w-8xl"> 
+            <img src="01.png" className="rounded-xl z-10 scale-95 hover:scale-100 transition w-full max-w-3xl duration-200 shadow-xl"/>
+            <img src="02.png" className="rounded-xl z-10 scale-95 hover:scale-100 transition w-full max-w-3xl duration-200 shadow-xl"/>
+            <img src="03.png" className="rounded-xl z-10 scale-95 hover:scale-100 transition w-full max-w-3xl duration-200 shadow-xl"/>
           </div> 
-          <Crew gothicFont={gothicFont}/>
+          <div className="grid grid-cols-2 justify-items-center p-5 [direction:rtl] mx-8">
+          <Crew font={gothicFont}/>
+          </div>
         </div>
       </div>
     </div>
